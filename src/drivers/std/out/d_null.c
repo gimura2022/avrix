7be5872e9f6caf51f4a4638fe9d1011ed63d012d
gimura2022 <gimura0001@gmail.com>
2024-09-12 16:48:58 +0300

    added auto return to d_null in out thread

--- src/drivers/std/out/d_null.c
+++ src/drivers/std/out/d_null.c
@@ -3,4 +3,14 @@
 FILE d_out = FDEV_SETUP_STREAM(D_OUT_PutChar, NULL, _FDEV_SETUP_WRITE);
 
 void D_OUT_Init(void) {}
-void D_OUT_PutChar(char c, FILE* stream) {}
+
+void D_OUT_PutChar(char c, FILE* stream) {
+    switch (c) {
+        case '\n':
+            D_OUT_PutChar('\r', stream);
+            break;
+
+        default:
+            break;
+    }
+}
